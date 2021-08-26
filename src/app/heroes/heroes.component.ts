8bfee6232404cad1c71b0d558fc796952bc55564
Adriano <adriano.henrique.eng@gmail.com>
2021-08-26 10:25:06 -0300

    adding a hero property and showing the hero (via element selector app-heroes)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
+  hero = 'Windstorm';
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }

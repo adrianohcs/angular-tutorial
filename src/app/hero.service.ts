f1f35347dbf8d31fb112b7955b81287424496985
Adriano <adriano.henrique.eng@gmail.com>
2021-08-26 14:12:38 -0300

    adding new heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -49,6 +49,14 @@ export class HeroService {
       );
   }
 
+  /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
   /** GET hero by id. Will 404 if id not found */
   getHero(id: number): Observable<Hero> {
     const url = `${this.heroesUrl}/${id}`;
